e494d39632cda3704b01c9f101a05816434f8ff0
Md. Abdullah Al-Mamoon <almamoon18@gmail.com>
2023-02-06 21:03:16 +0600

    code update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,62 +12,66 @@ import { FaUserMd, FaMoneyBillWave, FaLock } from "react-icons/fa";
 import { ImProfile } from "react-icons/im";
 import { RiDraftLine } from "react-icons/ri";
 import { Link } from 'react-router-dom';
+import { FiSearch } from "react-icons/fi";
+
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+
+// import Sonnet from '../../components/Sonnet';
 
 const Home = () => {
     return (
         <div className='p-1 bg-white text-black'>
-            <div className="row">
+            <div className="row g-0">
                 <div className="col col-md-3">
-                    <div className='menu-headers ubuntu'>
-                        <ul className='d-flex mb-3'>
-                            <li className='ubuntu fw-semibold' id='menu-home' style={{color: 'black'}}>Menu</li>
-                            <li className='ms-3' id='menu-manu' style={{color: 'black'}}><HiOutlineSquares2X2></HiOutlineSquares2X2></li>
-                            <li className='ms-3' id='menu-chats' style={{ color: 'black' }}><SlBookOpen></SlBookOpen></li>
-                            <li className='ms-3' id='menu-settings' style={{ color: 'black' }}><IoSettingsOutline></IoSettingsOutline></li>
-                        </ul>
-                    </div>
 
-                    <div className='menu-items'>
-                        <ul>
-                            <li className='p-2'>
-                                <Link to='/'><MdOutlineHome className='fs-4 me-3'></MdOutlineHome> Dashboard</Link>
-                            </li>
-                            <li className='p-2'>
-                                <Link to=''><VscPersonAdd className='fs-4 me-3'></VscPersonAdd>Create A New Form</Link>
-                            </li>
-                            <li className='p-2'>
-                                <Link><TfiMenuAlt className='fs-4 me-3'></TfiMenuAlt>Case Form History</Link>
-                            </li>
-                            <li className='p-2'>
-                                <Link><BsCircleHalf className='fs-4 me-3'></BsCircleHalf>Drafts</Link>
-                            </li>
-                            <li className='p-2'>
-                                <Link><CgUserList className='fs-4 me-3'></CgUserList>Chat List</Link>
-                            </li>
-                            <li className='p-2'>
-                                <Link><FaUserMd className='fs-4 me-3'></FaUserMd>Doctor List</Link>
-                            </li>
-                            <li className='p-2'>
-                                <Link><RiDraftLine className='fs-4 me-3'></RiDraftLine>Bill Info</Link>
-                            </li>
-                            <li className='p-2'>
-                                <Link><FaMoneyBillWave className='fs-4 me-3'></FaMoneyBillWave>Notification</Link>
-                            </li>
-                            <li className='p-2'>
-                                <Link><ImProfile className='fs-4 me-3'></ImProfile>Profile</Link>
-                            </li>
-                            <li className='p-2'>
-                                <Link><FaLock className='fs-4 me-3'></FaLock>Change Password</Link>
-                            </li>
-                            <li className='p-2'>
-                                <Link><IoExitOutline className='fs-4 me-3'></IoExitOutline>Logout</Link>
-                            </li>
-                        </ul>
-                    </div>
+                    <Tabs
+                        defaultActiveKey="menu"
+                        id="uncontrolled-tab-example"
+                        className="mb-3"
+                    >
+                        <Tab eventKey="menu" className='ubuntu fw-semibold' title="Menu">
+
+                            <div className='menu-items'>
+                                <Link to='/'><MdOutlineHome className='fs-4 me-3'></MdOutlineHome>Dashboard</Link>
+                                <Link to='/'><VscPersonAdd className='fs-4 me-3'></VscPersonAdd>Create A New Form</Link>
+                                <Link to='/'><TfiMenuAlt className='fs-4 me-3'></TfiMenuAlt>Case Form History</Link>
+                                <Link to='/'><BsCircleHalf className='fs-4 me-3'></BsCircleHalf>Drafts</Link>
+                                <Link to='/'><CgUserList className='fs-4 me-3'></CgUserList>Chat List</Link>
+                                <Link to='/'><FaUserMd className='fs-4 me-3'></FaUserMd>Doctor List</Link>
+                                <Link to='/'><RiDraftLine className='fs-4 me-3'></RiDraftLine>Bill Info</Link>
+                                <Link to='/'><FaMoneyBillWave className='fs-4 me-3'></FaMoneyBillWave>Notification</Link>
+                                <Link to='/'><ImProfile className='fs-4 me-3'></ImProfile>Profile</Link>
+                                <Link to='/'><FaLock className='fs-4 me-3'></FaLock>Change Password</Link>
+                                <Link to='/'><IoExitOutline className='fs-4 me-3'></IoExitOutline>Logout</Link>
+                            </div>
+                        </Tab>
+
+
+                        <Tab eventKey="elements" title=<HiOutlineSquares2X2></HiOutlineSquares2X2>>
+
+                        </Tab>
+
+                        <Tab eventKey="chats" title=<SlBookOpen></SlBookOpen> >
+                            <div>
+                                <div className='search-box d-flex justify-content-center'>
+                                    <button className='search-button btn btn-dark'><FiSearch ></FiSearch></button>
+                                    <input type="text" className='w-75' name="" id="" placeholder='Search...' />
+                                </div>
+                            </div>
+                        </Tab>
+
+                        <Tab eventKey="settings" title=<IoSettingsOutline></IoSettingsOutline> >
+                            <div className='ps-3'>
+                                <p className='ubuntu fw-semibold'>Choose Skin</p>
+                            </div>
+                        </Tab>
+                    </Tabs>
+                    
                 </div>
 
                 <div className="col col-md-9">
-
+                    
                 </div>
             </div>
         </div>
